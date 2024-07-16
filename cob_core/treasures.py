f9db5d4937b3586ff735091f9aaa397b16ce535a
Marcin Baryłka <marcin.barylka@gmail.com>
2024-07-16 12:25:56 +0200

    treasures rolling

--- cob_core/treasures.py
+++ cob_core/treasures.py
@@ -1,14 +1,15 @@
 import enum
 from dataclasses import dataclass
+from typing import Any
 
-from cob_core.dice import roll
+from cob_core.dice import Dice, roll
 from cob_core.weapons import Ax, Bow, Dagger, Hammer, Sword, ThrowDagger
 
-JEWELERY = [0, 1, 5, 10, 15, 20, 25, 35, 50, 75, 100, 150]
+JEWELERY = [1, 5, 10, 15, 20, 25, 35, 50, 75, 100, 150]
 MAGIC_ITEM_TYPES = ["weapon", "armor", "potion", "talisman", "medallion", "ring"]
 
 # TODO: Implement magic items
-MAGIC_ITEM = {
+MAGIC_ITEMS = {
     "weapon": [Sword(), Hammer(), Ax(), Bow(), Dagger(), ThrowDagger()],
     "armor": [1, 1, 1, 2, 2, None],
     "potion": [
@@ -48,7 +49,7 @@ class Treasure(enum.Enum):
     F = TreasureItems("3:1D6*5", "3:1D3", "1:1")
     G = TreasureItems("6:1D6*5", "3:1D6", "2:1")
     H = TreasureItems("6:2D6", "3:1D3", "1:1")
-    I = TreasureItems("6:1D6*5", "2:1D6", "2:1")
+    I = TreasureItems("6:1D6*5", "2:1D6", "2:1")  # noqa
     J = TreasureItems("6:1D6*20", "2:1D6", "3:1D3")
     K = TreasureItems("6:2D6*20", "3:1D6", "3:1D3")
     L = TreasureItems("6:3D6*20", "4:1D6", "4:1D3")
@@ -71,10 +72,50 @@ class Treasure(enum.Enum):
         if not has_jewelery:
             return []
         return (
-            [JEWELERY[roll("2d6")] for _ in range(roll(dice_code))] if dice_code else []
+            [JEWELERY[roll("2d6") - 2] for _ in range(roll(dice_code))]
+            if dice_code
+            else []
         )
 
-    def roll_magic_items(self): ...
+    def roll_magic_items(self) -> dict[str, list[Any]]:
+        """Roll for magic items."""
+
+        result = {key: [] for key in MAGIC_ITEM_TYPES}
+        probability, code = Treasure.parse_treasure_code(self.value.magic_items)
+
+        # Try to parse dice code. If it fails, that means there is arbitrary value.
+        arbitrary_value = 0
+
+        if not code:
+            return result
+
+        has_magic_items = roll("d6") <= probability if probability else False
+        if not has_magic_items:
+            return result
+
+        try:
+            _ = Dice(code)
+        except ValueError:
+            arbitrary_value = int(code)
+            code = None
+
+        if code:
+            arbitrary_value = roll(code)
+
+        # TODO implement 'roll twice' when armor & 6.
+        for _ in range(arbitrary_value):
+            magic_item_type = MAGIC_ITEM_TYPES[roll("d6") - 1]
+            magic_item = MAGIC_ITEMS[magic_item_type][roll("d6") - 1]
+            result[magic_item_type].append(magic_item)
+        return result
+
+    def get_treasure(self) -> dict[str, Any]:
+        """Get treasure."""
+        return {
+            "gold": self.roll_gold(),
+            "jewelery": self.roll_jewelery(),
+            "magic_items": self.roll_magic_items(),
+        }
 
     @staticmethod
     def parse_treasure_code(code: str) -> tuple[int, str] | tuple[int, None]:
