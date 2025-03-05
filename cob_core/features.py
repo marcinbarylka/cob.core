bf26e912f3280c4edce5a5cc98cb25b88e2656cc
Marcin Baryłka <marcin.barylka@tylko.com>
2025-03-05 09:25:41 +0100

    Fix no return error in random_feature, refactor reminder

--- cob_core/features.py
+++ cob_core/features.py
@@ -27,10 +27,15 @@ class FeatureFactory:
             a random feature or None
 
         """
-        if random.randint(1, 80) > 50:  # 62.5% chance to have a feature in theory, but we use 50% for simplicity
+        # todo: refactor me
+        if (
+            random.randint(1, 80) > 50
+        ):  # 62.5% chance to have a feature in theory, but we use 50% for simplicity
             return None
 
-        total_variants = sum(len(variants) for variants in FeatureFactory.FEATURE_TABLE.values())
+        total_variants = sum(
+            len(variants) for variants in FeatureFactory.FEATURE_TABLE.values()
+        )
         random_variant_number = random.randint(1, total_variants)
 
         for feature_type, variants in FeatureFactory.FEATURE_TABLE.items():
@@ -40,6 +45,7 @@ class FeatureFactory:
                 feature_class = globals().get(feature_class_name)
                 return feature_class() if feature_class else None
             random_variant_number -= len(variants)
+        return None
 
 
 class Feature: ...
