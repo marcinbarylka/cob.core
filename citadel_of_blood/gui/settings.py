5971b2407b88970b3a4fc08e0468e1cbe2b29dda
Marcin Baryłka <marcin.barylka@gmail.com>
2025-06-16 18:13:44 +0200

    Remove unnecessary setting

--- citadel_of_blood/gui/settings.py
+++ citadel_of_blood/gui/settings.py
@@ -8,7 +8,7 @@ import platformdirs
 import toml
 from pydantic import BaseModel, Field, field_validator
 
-from citadel_of_blood.constants import PROJECT_NAME, PROJECT_ROOT
+from citadel_of_blood.constants import PROJECT_NAME
 from citadel_of_blood.errors.gui_errors import InvalidHexColorError, NonPositiveDimensionError, NonPositiveFPSError
 
 S = TypeVar("S", bound="Settings")
@@ -62,7 +62,6 @@ class GUISettings(BaseModel):
     sfx_enabled: bool = True
     music_enabled: bool = True
     cache_folder: Path | str = Field(default_factory=lambda: Path(platformdirs.user_cache_path(PROJECT_NAME)))
-    default_font_path: Path = Field(default_factory=lambda: Path(PROJECT_ROOT / "assets/fonts/Roboto-Regular.ttf"))
 
     @field_validator("width", "height")
     @classmethod
@@ -100,7 +99,7 @@ class GUISettings(BaseModel):
             raise NonPositiveFPSError()
         return v
 
-    @field_validator("basic_font", "cache_folder", "default_font_path", mode="before")
+    @field_validator("basic_font", "cache_folder", mode="before")
     @classmethod
     def ensure_path(cls, v: Path | str) -> Path:
         """Convert string paths to Path objects before validation.
