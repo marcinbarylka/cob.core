8affa6a931a2e381c80895eea2a4695cf428652e
Marcin Baryłka <marcin.barylka@gmail.com>
2025-12-08 18:36:35 +0100

    Add temporary screen init

--- citadel_of_blood/main.py
+++ citadel_of_blood/main.py
@@ -11,17 +11,20 @@ def run():
     """Run the GUI."""
     game = Game()
     game.init_gui()
+    font_path = f"{BDF_FOLDER}/10x20.bdf"
+    font_measurements = FramedConsole.measure_bdf(font_path)
     manager = ConsoleManager()
     console1 = manager.create_console(
         type=FramedConsole,
-        font=f"{BDF_FOLDER}/iv8x16u.bdf",
-        width=1024 // 8,
-        height=768 // 16,
+        font=font_path,
+        width=1024 // font_measurements[0],
+        height=768 // font_measurements[1],
         x=0,
         y=0,
-        background=(0xa0, 0xa0, 0xa0),
+        background=(0xA0, 0xA0, 0xA0),
         foreground=(0x00, 0x00, 0x00),
     )
+    console1.print(1, 1, "Hello, Citadel of Blood!")
     manager.render_all(game.surface)
     game.run()
 
