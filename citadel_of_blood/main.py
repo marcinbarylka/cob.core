6e69abc841051de7a6b4fc4f362103998ede48c3
Marcin Baryłka <marcin.barylka@gmai.com>
2025-12-08 16:28:33 +0100

    Add create_console, fix error in lib

--- citadel_of_blood/main.py
+++ citadel_of_blood/main.py
@@ -1,5 +1,7 @@
 """Main module for the game."""
 
+from tileconsole import BDF_FOLDER
+from tileconsole.console import FramedConsole
 from tileconsole.manager import ConsoleManager
 
 from citadel_of_blood.gui import Game
@@ -7,12 +9,21 @@ from citadel_of_blood.gui import Game
 
 def run():
     """Run the GUI."""
-    g = Game()
-    g.init_gui()
-
-    cm = ConsoleManager()
-
-    g.run()
+    game = Game()
+    game.init_gui()
+    manager = ConsoleManager()
+    console1 = manager.create_console(
+        type=FramedConsole,
+        font=f"{BDF_FOLDER}/iv8x16u.bdf",
+        width=1024 // 8,
+        height=768 // 16,
+        x=0,
+        y=0,
+        background=(0xa0, 0xa0, 0xa0),
+        foreground=(0x00, 0x00, 0x00),
+    )
+    manager.render_all(game.surface)
+    game.run()
 
 
 if __name__ == "__main__":
